1a2208182911b157fa86f05d7bbee89aa17307ae
bsautermeister <apps@bsautermeister.de>
2017-06-24 22:54:10 +0200

    Moved keras model in separate file

--- ml/emotion_detector/keras_training.py
+++ ml/emotion_detector/keras_training.py
@@ -1,31 +1,13 @@
 from __future__ import print_function
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+
 from keras.preprocessing.image import ImageDataGenerator
+
+from models.dexpression import model_keras
 
 batch_size = 64
 image_size = 48
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(image_size, image_size, 1)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(7))
-model.add(Activation('sigmoid'))
+model = model_keras.create_model(image_size)
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
